cd6fb75d84e46cd024527f4f12de87acc4045aa8
kriperi <mikkelfs@gmail.com>
2021-10-21 10:18:48 +0200

    Connected database

---
+++ src/main/java/no/kristiania/person/HelloDatabase.java
@@ -0,0 +1,33 @@
+package no.kristiania.person;
+
+import org.postgresql.ds.PGSimpleDataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class HelloDatabase {
+    public static void main(String[] args) throws SQLException {
+
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/guy_db");
+        dataSource.setUser("guy");
+        dataSource.setPassword("Ax2rw])rJdJJ.5vK");
+
+        try (Connection connection = dataSource.getConnection()) {
+            //Statement for å gi kommandoer til databasen
+            try (PreparedStatement statement = connection.prepareStatement("select * from people")) {
+
+                //Resultatset for å få lese ut resultater fra databsen
+                try (ResultSet rs = statement.executeQuery()) {
+                    while (rs.next()) {
+                        System.out.println(rs.getString(2));
+                    }
+                }
+
+            }
+        }
+
+    }
+}
